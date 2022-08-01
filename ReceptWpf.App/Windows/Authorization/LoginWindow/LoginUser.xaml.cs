73df9eee3a0665c290b49bcb57dc7b0ad14fa0c9
BairamovAllaz <ellezbayramov2004@gmail.com>
2022-08-02 00:04:06 +0300

    Adding LoginWindow

---
+++ ReceptWpf.App/Windows/Authorization/LoginWindow/LoginUser.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using ReceptWpf.App.Windows.Authorization.RegisterWindow;
+
+namespace ReceptWpf.App.Windows.Authorization.LoginWindow;
+
+public partial class LoginUser : Window
+{
+    public LoginUser()
+    {
+        InitializeComponent();
+    }
+
+    private void Hyperlink_OnClick(object sender, RoutedEventArgs e)
+    {
+        new RegisterUser().Show();
+        this.Close();
+    }
+}
---
+++ ReceptWpf.App/Windows/Authorization/RegisterWindow/RegisterUser.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using ReceptWpf.App.Windows.Authorization.LoginWindow;
+
+namespace ReceptWpf.App.Windows.Authorization.RegisterWindow;
+
+public partial class RegisterUser : Window
+{
+    public RegisterUser()
+    {
+        InitializeComponent();
+    }
+
+    private void Hyperlink_OnClick(object sender, RoutedEventArgs e)
+    {
+        new LoginUser().Show();
+        this.Close();
+    }
+}
