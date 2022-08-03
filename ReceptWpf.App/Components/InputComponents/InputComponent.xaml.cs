4d518eebdcde85b530453a78cf193ace8a8d2f40
BairamovAllaz <ellezbayramov2004@gmail.com>
2022-08-03 08:18:04 +0300

    Fixed Data triggers Input boxes

--- ReceptWpf.App/Components/InputComponents/InputComponent.xaml.cs
+++ ReceptWpf.App/Components/InputComponents/InputComponent.xaml.cs
@@ -7,6 +7,7 @@ namespace ReceptWpf.App.Components.InputComponents;
 public partial class InputComponent : UserControl,INotifyPropertyChanged
 {
     private string _labelname;
+    private bool _isempty;
 
     public string LabelName
     {
@@ -19,15 +20,38 @@ public partial class InputComponent : UserControl,INotifyPropertyChanged
             OnPropertyChanged(nameof(LabelName));
         }
     }
+
+    public bool IsEmpty
+    {
+        get => _isempty;
+        set
+        {
+            if (_isempty == value) return;
+            _isempty= value;
+            OnPropertyChanged(nameof(IsEmpty));               
+        }
+    }
+    private void InputMy_OnTextChanged(object sender, TextChangedEventArgs e)
+    {
+        if (InputMy.Text.Length > 0)
+        {
+            IsEmpty = true;
+        }
+        else
+        {
+            IsEmpty = false;
+        }
+    }
     public InputComponent()
     {
         InitializeComponent();
     }
-
     public event PropertyChangedEventHandler? PropertyChanged;
 
     protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
     {
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
+
+   
 }
