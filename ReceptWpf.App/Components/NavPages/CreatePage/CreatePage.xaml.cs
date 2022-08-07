a98cafa0e2b8da8f1f38cb1a87e7ae66d4fe9254
BairamovAllaz <ellezbayramov2004@gmail.com>
2022-08-07 08:34:39 +0300

    Added default image

--- ReceptWpf.App/Components/NavPages/CreatePage/CreatePage.xaml.cs
+++ ReceptWpf.App/Components/NavPages/CreatePage/CreatePage.xaml.cs
@@ -10,30 +10,24 @@ namespace ReceptWpf.App.Components.NavPages.CreatePage;
 
 public partial class CreatePage : UserControl
 {
-    private string _photo;
+    private string? _photo;
     public Models.UserDB.User_Models.User User{ get; set; }
     public CreatePage()
     {
         //TODO FIX IMAGE PROBLEM
-        _photo  = @"C:\Users\ellez\OneDrive\Programing\Csharp\ReceptWpf\ReceptWpf.App\Components\NavPages\CreatePage";
         InitializeComponent();
-        PhotoPlace.Source = new BitmapImage(new Uri(_photo));
+        var url = $@"{Directory.GetCurrentDirectory()}\default2.png";
+        PhotoPlace.Source = new BitmapImage(new Uri(url));
     }
-    
-    /*
-     * Pice of the code for future
-     private void ButtonSave_OnClick(object sender, RoutedEventArgs e)
-         {
-             var ext = _photo.Substring(_photo.LastIndexOf('.'));
-             File.Copy(_photo, $@"D:\photos\{_account.Id}_{_user.LastName}_{_user.FirstName}{ext}", true);
-         }
-     */
     private void ChangePhotoButton_OnClick(object sender, RoutedEventArgs e)
     {
         var file = new OpenFileDialog();
         file.Filter = "Image files (*.png;*.jpeg)|*.png;*.jpeg|All files (*.*)|*.*";
         file.ShowDialog();
         _photo = file.FileName;
-        PhotoPlace.Source = new BitmapImage(new Uri(_photo));
+        if(_photo is null)
+        {
+            PhotoPlace.Source = new BitmapImage(new Uri(_photo));
+        }
     }
 }
