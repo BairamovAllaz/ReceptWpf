c836160728c47f12dce5ee367dc67ccd42937bf7
BairamovAllaz <ellezbayramov2004@gmail.com>
2022-08-14 23:44:26 +0300

    Styling done ActuallComponentShow.xaml

--- ReceptWpf.App/Components/NavPages/ActuallComponentShow/ActuallComponentShow.xaml.cs
+++ ReceptWpf.App/Components/NavPages/ActuallComponentShow/ActuallComponentShow.xaml.cs
@@ -1,23 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
 using System.Windows.Media.Imaging;
 using Models.FoodDB.FoodModels;
 using ReceptWpf.App.Components.NavPages.Show;
-
 namespace ReceptWpf.App.Components.NavPages.ActuallComponentShow;
-
 public partial class ActuallComponentShow : UserControl
 {
     public Food food { get; set; }
+    public List<string> IngredientsList { get; set; }
     public ActuallComponentShow()
     {
         food = Home.Home.Foood;
+        IngredientsList = GetIngredients();
         InitializeComponent();
         ImageStack.Source = new BitmapImage(new Uri(food.FoodPhoto));
     }
     private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
     {
         MessageBox.Show(food.ToString());
+    }
+    public List<string> GetIngredients()
+    {
+        List<string> fake = new List<string>();
+        string[] list = food.Ingredients.Split(',');
+        foreach (var s in list)
+        {
+            fake.Add(s);
+        }
+        return fake;
     }
 }
